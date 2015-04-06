620aef91ccaa7e6fd86530e11f52a359016b2c6e
Daniel Tyson <danielctyson@gmail.com>
2015-04-06 15:01:29 -0400

    Playlists are created for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_create :set_token
+  after_create :make_playlist
 
   has_many :playlists, dependent: :destroy
 
   private
+  def make_playlist
+    playlist = Playlist.create(name: "My Queue", list_type: 0)
+    self.playlists << playlist
+  end
+
   def set_token
     return if token.present?
     self.token = generate_token
