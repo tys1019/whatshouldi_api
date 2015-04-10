035658e1c5ee9f62dd4fbf0adf7d8d08a360c43f
Daniel Tyson <danielctyson@gmail.com>
2015-04-10 11:40:24 -0400

    Simplified playlists for readability

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,6 +1,14 @@
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :list_type
+  attributes :id, :list_type, :shows_simple
   has_many :movies
-  has_many :shows
+  # has_many :shows
+
+  def shows_simple
+     @object.shows.map do |show|
+
+      show['seasons'] = nil
+      show
+     end
+  end
 
 end
