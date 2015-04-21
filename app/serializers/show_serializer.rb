fff8a50e4fe191225e0ad2d4bc65ea72c52ba67d
Daniel Tyson <danielctyson@gmail.com>
2015-04-20 21:26:32 -0400

    Added related to serializers

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,3 +1,3 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes  :title, :overview, :poster_path, :runtime, :guidebox_id, :artwork, :rottentomatoes_id, :imdb_id, :themoviedb_id, :cast, :purchase_web_sources, :free_web_sources, :subscription_web_sources
+  attributes  :title, :overview, :poster_path, :runtime, :guidebox_id, :artwork, :rottentomatoes_id, :imdb_id, :themoviedb_id, :cast, :purchase_web_sources, :free_web_sources, :subscription_web_sources, :related
 end
